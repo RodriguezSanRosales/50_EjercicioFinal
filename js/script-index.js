8ec68805148baf0e5fe4755f1159ce5e5590e315
RodriguezSanRosales <san.rodzrosales@gmail.com>
2016-12-05 21:05:54 +0000

    etapa 7

--- js/script-index.js
+++ js/script-index.js
@@ -1,4 +1,5 @@
-$(document).on("ready", function(){
+// $(document).on("ready", function(){
+$(document).ready(function() {	
 
 	$('.js-back').hide();
 
@@ -6,12 +7,13 @@ $(document).on("ready", function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	renderRecipe(recipe);
 
 });
 
 function printNews(){
-		$('.callout-news p').append("NUEVAS RECETAS")
-	}
+	$('.callout-news p').append("NUEVAS RECETAS")
+}
 	
 
 /*
@@ -21,11 +23,12 @@ function printNews(){
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
 
-	for(i=0; i<=recipesArray.length; i++){
+	for(var i=0; i < recipesArray.length; i++) {
 		if(recipesArray[i].highlighted == true){
 			renderRecipe(recipesArray[i]); 
 		}
 	}
+
 }
 
 /*
@@ -36,7 +39,52 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
-}
+
+	var a = $('<a/>');
+	a.addClass('item-recipe');
+
+	var span = $('<span></span>');
+	span.addClass('attribution');
+
+	var spanTittle = $('<span></span>');
+	spanTittle.addClass('title-recipe');
+	spanTittle.attr('tittle');
+
+	var spanMeta = $('<span></span>');
+	spanMeta.addClass('metadata-recipe');
+
+	var spanAuthor = $('<span></span>');
+	spanAuthor.addClass('author-recipe');
+	spanAuthor.text(name);
+
+	var spanBook = $('<span></span>');
+	spanBook.addClass('bookmarks-recipe');
+
+	var spanIcon = $('<span></span>');
+	spanIcon.addClass('icon-bookmark');
+
+	var img = $('<img/>');
+	img.attr('src', 'https://s-media-cache-ak0.pinimg.com/originals/27/3a/02/273a023d4c06cb95eca6eb667ce3c68e.jpg');
+
+	//metemos span dentro de a
+	a.append(span);
+
+	//metemos spans dentro de span
+	span.append(spanTittle);
+	span.append(spanMeta);
+
+	//metemos spans dentro de spanMeta
+	spanMeta.append(spanAuthor);
+	spanMeta.append(spanBook);
+
+	//metemos spans dentro de spanBook
+	spanBook.append(spanIcon);
+
+	//pintarlos en pantalla
+	console.log(a);
+	$('.list-recipes').append(a);
+};
+
 
 
 
--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,5 @@
 $(document).ready( function(){
+	console.log('documentom listo');
 
 	$('.js-menu').hide();
 
